14c3e22d6b5cb6eac286b7de1b90df6e1ef65b06
Shani Henriques <shanihenriques@gmail.com>
2022-02-16 22:22:13 +0000

    random distribution: initial commit

---
+++ 2 random_distribution/sketch.js
@@ -0,0 +1,31 @@
+let bar_array;
+
+function setup() {
+  createCanvas(1200, 450);
+  background(255, 204, 0);
+  bar_array = new Array(20).fill(0); // create array of size 20 with 0 as every value
+}
+
+function draw() {
+  // pick a random bar to increase in value
+  let idx = floor(random(20));
+  bar_array[idx]++;
+
+  stroke(0);
+  strokeWeight(2);
+  fill(255);
+
+  let bar_width = width / bar_array.length;
+
+  for (let i = 0; i < bar_array.length; i++) {
+    
+    rect( 
+      i * bar_width, // x at top left corner of each bar
+      height - bar_array[i], // y at height of the window - current bar height 
+      bar_width, // width = width
+      bar_array[i] // height = same as it currently is 
+    ); 
+  
+  }
+}
+
